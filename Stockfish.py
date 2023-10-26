d4e5964ec785ec814e179821faea4199a00c70fe
James Claar <jamesdclaar@gmail.com>
2023-10-25 21:56:02 -0500

    Added Linux stockfish and auto-detect OS

--- Stockfish.py
+++ Stockfish.py
@@ -1,6 +1,20 @@
-from stockfish import Stockfish # Documentation: https://pypi.org/project/stockfish/
-import chess                    # Documentation: https://python-chess.readthedocs.io/en/latest/
+from stockfish import Stockfish # (pip install stockfish) Documentation: https://pypi.org/project/stockfish/
+import chess                    # (pip install python-chess) Documentation: https://python-chess.readthedocs.io/en/latest/
 
-stockfish = Stockfish("stockfish-16-windows\\stockfish-windows-x86-64-avx2.exe")
+import os
+
+# detect whether we're on windows or linux
+STOCKFISH_PATH = {
+    'windows': 'stockfish-16-windows\\stockfish-windows-x86-64-avx2.exe',
+    'linux': 'stockfish-16-linux/src/stockfish'
+}
+
+os_name = os.name
+if (os_name == 'nt'): # Windows
+    current_os = 'windows'
+else:
+    current_os = 'linux'
+
+stockfish = Stockfish(STOCKFISH_PATH[current_os])
 
 print(stockfish.get_stockfish_major_version())
