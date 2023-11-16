794f0c7c64c4dc0a411e22ae71f1c257362b31cd
james-claar <jamesdclaar@gmail.com>
2023-11-15 18:43:17 -0600

    new test

--- Controller.py
+++ Controller.py
@@ -105,6 +105,11 @@ try:
         Xvel = joystickInputs[0] * SPEED_MULTIPLIER
         Yvel = joystickInputs[1] * SPEED_MULTIPLIER
 
+        setDirections(Xvel, Yvel)
+
+        Xvel = abs(Xvel)
+        Yvel = abs(Yvel)
+
         if controller.get_button(0): # If A is pressed, calibrate
             calibrate()
 
