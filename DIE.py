83b9913ff46881d9305b67d60245ec146513fcc9
james-claar <jamesdclaar@gmail.com>
2023-11-29 17:57:14 -0600

    basic movement

--- DIE.py
+++ DIE.py
@@ -6,9 +6,9 @@ PIN_EN = 22
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN_EN, GPIO.OUT)
 
-# Turn EN on and off 10 times / sec
+# Turn EN on and off 100 times / sec
 
-for i in range(100):
+for i in range(1000):
     GPIO.output(PIN_EN, 1)
     sleep(0.005)
     GPIO.output(PIN_EN, 0)
