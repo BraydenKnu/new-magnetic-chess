737e1144ef13a928d74f020ed899e9f764d6d6ed
james-claar <jamesdclaar@gmail.com>
2023-11-29 15:50:08 -0600

    basic movement

--- BasicMovementTest.py
+++ BasicMovementTest.py
@@ -58,12 +58,10 @@ def multi_steps(motor, direction, steps):
 
 try:
     # do a diamond shape
-    """
-    multi_steps(0, 1, STEPS_PER_REV)
-    multi_steps(1, 1, STEPS_PER_REV)
-    multi_steps(0, 0, STEPS_PER_REV)
-    multi_steps(1, 0, STEPS_PER_REV)
-    """
+    multi_steps(0, 1, STEPS_PER_REV/2)
+    multi_steps(1, 1, STEPS_PER_REV/2)
+    multi_steps(0, 0, STEPS_PER_REV/2)
+    multi_steps(1, 0, STEPS_PER_REV/2)
     
     # Test EN pin on and off
     for i in range(3):
