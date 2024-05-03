ee73b0419a804ec329c12b78be98a96aa617a2a1
James Claar <jamesdclaar@gmail.com>
2024-05-03 12:04:04 -0500

    Senior Design Showcase

--- MagneticChessPython/main.py
+++ MagneticChessPython/main.py
@@ -2,9 +2,36 @@
 Continually updates ChessInterface.
 """
 
-from ChessInterface import ChessInterface
+PLAYING_GAME = 1
 
+from ChessInterface import ChessInterface
 interface = ChessInterface()
 
-while True:
-    interface.update()
+"""
+programmer_input = "illegal command"
+while programmer_input != "":
+    programmer_input = input("Press Enter to start. Type c to config. Type s to start immediately without user input.")
+
+    if programmer_input == "c":
+        success = False
+        while not success:
+            try:
+                white = input("Enter black's elo (leave blank for maximum): ")
+                black = input("Enter white's elo (leave blank for maximum): ")
+                white = None if white == "" else int(white)
+                black = None if black == "" else int(black)
+                interface.setWhiteElo(white)
+                interface.setBlackElo(black)
+                success = True
+            except KeyboardInterrupt:
+                exit()
+            except:
+                print("Invalid input, try again.")
+    elif programmer_input == "s":
+        interface.update(sendCommands=True)
+        interface.gameEnded = False
+        interface.state = PLAYING_GAME
+        break
+"""
+
+interface.loop()
